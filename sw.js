6506a297d2240fc70a641869002cb924bcb730eb
medaka-kazu <kazu10128501@gmail.com>
2026-07-22 08:01:31 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,13 +1,1 @@
-const CACHE_NAME = 'mallet-golf-score-v15';
-const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
-  self.clients.claim();
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
-});
+self.addEventListener('fetch',()=>{});
